97c215445c01cb51152a7ed8b76bfc3a01200a3d
moeabdol <mohd.a.saed@gmail.com>
2018-11-14 22:01:03 +0300

    Create basic express server and connect to database

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const db = require('./models');
+
+const app = express();
+
+db.sequelize
+  .sync()
+  .then(() => {
+    app.listen(3000, () => console.log('Server listening on port 3000'));
+  })
+  .catch(err => console.error(err));
